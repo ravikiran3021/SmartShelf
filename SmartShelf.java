c8af207f4371dd13ef0dbf830b025de1d805af46
RAVI KIRAN EEDUPUGANTI <103324437+ravikiran3021@users.noreply.github.com>
2025-08-16 14:55:19 +0530

    Update SmartShelf.java

--- SmartShelf.java
+++ SmartShelf.java
@@ -1,1 +1,37 @@
+class Smartshelf 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do
+        {
+            System.out.println(ConsoleColors.BLUE + ConsoleColors.BOLD + "\nWelcome to SmartShelf!" + ConsoleColors.RESET);
+            System.out.println("1. Admin Login");
+            System.out.println("2. User Signup");
+            System.out.println("3. User Login");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
 
+            switch (choice) 
+            {
+                case 1:
+                    admin.adminLogin();
+                    break;
+                case 2:
+                    user.signup();
+                    break;
+                case 3:
+                    user.login();
+                    break;
+                case 4:
+                    System.out.println(ConsoleColors.GREEN + ConsoleColors.BOLD + "Thank You for using SmartShelf!" + ConsoleColors.RESET);
+                    return;
+                default:
+                    System.out.println(ConsoleColors.RED + "Invalid Option. Please try again." + ConsoleColors.RESET);
+            }
+        }while(choice !=4);
+    }
+}
